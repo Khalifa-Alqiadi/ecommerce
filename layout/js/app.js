7e0f18746ce0a5dd143b881185b569fd4cf39882
Khalifa-Alqiadi <khalifa.alqiadi@gmail.com>
2022-01-16 21:50:40 +0300

    commit init
    edited all files

---
+++ layout/js/app.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('#header .hamburger');
+const mobile_menu = document.querySelector('#header .header');
+// const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('header.container');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
